0f9eaa46d420a162e97496cbd0d0e2e694700002
williammacedo <Will.9455426>
2021-03-07 16:44:42 -0300

    Finish diceroller

--- app/src/main/java/br/com/williammacedo/diceroller/MainActivity.kt
+++ app/src/main/java/br/com/williammacedo/diceroller/MainActivity.kt
@@ -2,10 +2,41 @@ package br.com.williammacedo.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import br.com.williammacedo.diceroller.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var rollButton: Button
+    private lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        rollButton = binding.rollButton
+        diceImage = binding.diceImage
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
